d4e9cfe013f062abacdf3ae5d9a4b28af8bded5e
Markus Deutsch <md@karl-deutsch.at>
2019-10-28 16:11:49 +0100

    Added microphone button to main screen.

--- app/src/androidTest/java/at/moop/voicerecorder/MainActivityTest.kt
+++ app/src/androidTest/java/at/moop/voicerecorder/MainActivityTest.kt
@@ -3,7 +3,7 @@ package at.moop.voicerecorder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
@@ -21,8 +21,8 @@ class MainActivityTest {
     val rule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun testHelloWorldIsShown() {
-        onView(withText("Hello World!")).check(matches(isDisplayed()))
+    fun testRecordButtonIsShown() {
+        onView(withContentDescription(R.string.main_button_toggle_cd)).check(matches(isDisplayed()))
     }
 
 
